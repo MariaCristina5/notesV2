491e9f4f0de300fea37a1e5e42de992e56db7c48
MariaCristina5 <113431422+MariaCristina5@users.noreply.github.com>
2023-01-12 12:49:28 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const noteInput = document.querySelector('.note');
+const noteCointainerEl = document.querySelector('.note-container');
+const closeBtn = document.querySelector('.close-btn'); 
+
+// show/hide note controler container
+noteInput.addEventListener('click', ()=>{
+    hideFunction();
+    showFunction();
+})
+function hideFunction() {
+        let x = document.querySelector(".note");
+        if (x.style.display === "none") {
+        x.style.display = "block";
+        } else {
+        x.style.display = "none";
+        }
+    }
+function showFunction(){
+    let y = document.querySelector('.note-container');
+    if(y.style.display === "block") {
+        y.style.display = "none";
+    } else {
+        y.style.display = "block"
+    }
+}
+closeBtn.addEventListener('click', () =>{
+    noteCointainerEl.remove()
+
+})
