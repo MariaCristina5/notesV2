2a408eb5bf0a1a0f94fd73e040799273a461d1e1
MariaCristina5 <113431422+MariaCristina5@users.noreply.github.com>
2023-04-26 10:01:24 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,78 +1,72 @@
-const noteInput = document.querySelector('.note');//
-const noteContainerEl = document.querySelector('.note-container');//
-const closeBtn = document.querySelector('.close-btn'); //
-const controlerEl = document.querySelector('.main-controler')
-const notes = JSON.parse(localStorage.getItem('notes'));//
-const moreBtn = document.getElementById('.dropdown-content');
-const deleteBtn = document.getElementById('deleteNote');
-const noteTitle = document.querySelector('.note-title')//
-const form = document.getElementById('form');//
-const noteBody = document.querySelector('.note-body')//
-
-
-
-
-
-
+const noteInput = document.querySelector(".note"); //
+const noteContainerEl = document.querySelector(".note-container"); //
+const closeBtn = document.querySelector(".close-btn"); //
+const controlerEl = document.querySelector(".main-controler");
+const notes = JSON.parse(localStorage.getItem("notes")); //
+const moreBtn = document.getElementById(".dropdown-content");
+const deleteBtn = document.getElementById("deleteNote");
+const noteTitle = document.querySelector(".note-title"); //
+const form = document.getElementById("form"); //
+const noteBody = document.querySelector(".note-body"); //
+// const noteBodyContainer = document.querySelector(".note-box-container");
 
 // show/hide note controler container
-noteInput.addEventListener('click', ()=>{
-    hideFunction();
-    showFunction();
-   
-})
+controlerEl.addEventListener("click", () => {
+  console.log("am apasat");
+});
 
-closeBtn.addEventListener('click', ()=>{
-    hideFunction();
-    showFunction();
-    if(noteTitle !='' && noteBody !=''){
-        addNewNote();
-    }else {
-        alert('Empty note')
-    }
-})
+noteInput.addEventListener("click", () => {
+  hideFunction();
+  showFunction();
+});
 
+closeBtn.addEventListener("click", () => {
+  hideFunction();
+  showFunction();
 
-
-
-
-
-
-
+  if (noteBody.value != "" || noteTitle.value != "") {
+    addNewNote();
+  }
+  noteBody.value = "";
+  noteTitle.value = "";
+});
 
 function hideFunction() {
-    let x = document.querySelector(".note");
-    if (x.style.display === "none") {
+  let x = document.querySelector(".note");
+  if (x.style.display === "none") {
     x.style.display = "block";
-    } else {
+  } else {
     x.style.display = "none";
-    }
-
+  }
 }
-function showFunction(){
-let y = document.querySelector('.note-container');
-if(y.style.display === "block") {
+function showFunction() {
+  let y = document.querySelector(".note-container");
+  if (y.style.display === "block") {
     y.style.display = "none";
-} else {
-    y.style.display = "block"
+  } else {
+    y.style.display = "block";
+  }
 }
-}
-
+// credinta la romani dan puric
 
 //function for note container
-function addNewNote(){
-    controlerEl.innerHTML="";
-    const note = document.createElement('div');
-    note.classList.add('note-box-container')
+function addNewNote() {
+  controlerEl.innerHTML = "";
+  const note = document.createElement("div");
+  note.classList.add("note-box-container");
 
-    note.innerHTML = `
+  note.innerHTML = `
     <div class="note-box-header">
         <button class="btns pin-btn-small"><i class="fa-solid fa-thumbtack"></i></button>
         <button type="button" class="btns btn-small" id="note-close-btn">
                     <i class="fas fa-times"></i></button>
         <div class="span">
-            <span class="span-title">${document.querySelector('.note-title').value}</span><br>
-            <span>${document.querySelector('.note-body').value}</span>
+            <span class="span-title">${document
+              .querySelector(".note-title")
+              .value.slice(0, 10)}</span><br>
+            <span>${document
+              .querySelector(".note-body")
+              .value.slice(0, 15)}</span>
         </div>
     </div>
     <div class="more-tools-small">
@@ -94,21 +88,27 @@ function addNewNote(){
 
 
         `;
-        const noteCloseBtn = note.querySelector('#note-close-btn')
+  const noteCloseBtn = note.querySelector("#note-close-btn");
 
+  // controlerEl.appendChild(note);
+  document.querySelector("body").appendChild(note);
 
-    controlerEl.appendChild(note);
-    form.addEventListener('submit', (e) =>{
-        e.preventDefault();
-            let title = noteTitle.value;
-            let body = noteBody.value;
-           
-        console.log(localStorage.setItem(title, body))
-        
- });
+  // de editat notita
+  // note.addEventListener("click", () => {
+  //   showFunction();
+  //   hideFunction();
+  // note.remove()
+  // });
 
- //close the note container
- noteCloseBtn.addEventListener('click', ()=>{
+  //close the note container
+  noteCloseBtn.addEventListener("click", () => {
     note.remove();
-})
+  });
 }
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let title = noteTitle.value;
+  let body = noteBody.value;
+
+  console.log(localStorage.setItem(title, body));
+});
